bb9d76f7bd427c0177a36a3f2fe67bb520844710
pavanalakshmi <pavanalakshmi13579@gmail.com>
2024-10-07 19:50:31 -0400

    version 1 intial commit

--- src/multithreading/trading_multithreading/MultiThreadTradeRunner.java
+++ src/multithreading/trading_multithreading/MultiThreadTradeRunner.java
@@ -24,15 +24,17 @@ public class MultiThreadTradeRunner {
         try {
             Thread chunkProcessorThread = new Thread(chunkProcessor::chunksProcessor);
             chunkProcessorThread.start();
+            System.out.println("Chunk Processor started >");
 
             Thread chunkGeneratorThread = new Thread(() -> readTradeFile.readCSVGenerateChunks(filePath));
             chunkGeneratorThread.start();
-
+            System.out.println("Chunk Generator started >");
             chunkGeneratorThread.join();
             chunkProcessorThread.join();
 
             Thread tradeProcessorThread = new Thread(tradeProcessorService::processTrade);
             tradeProcessorThread.start();
+            System.out.println("Trade Processor started >");
             tradeProcessorThread.join();
 
         } catch (InterruptedException e) {
